14a4b962c4b70350a2f2f1f76680fc07c19bb7ae
Hayat <hayatu4islam@yahoo.com>
2023-01-15 17:36:13 +0000

    added middleware logger

--- myApp.js
+++ myApp.js
@@ -1,6 +1,12 @@
 let express = require('express');
 let app = express();
-// require("dotenv").config();
+require("dotenv").config();
+
+app.use(function(req, res, next) {
+    var string = req.method + " " + req.path + " - " + req.ip;
+    console.log(string);
+    next();
+})
 
 app.use("/public", express.static(__dirname + "/public"));
 // Meet the Node console
@@ -10,19 +16,19 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + "/views/index.html");
 });
 
-// app.get("/json", function(req, res){
-//     let msgObj = { message: "Hello json" };
-//     if (process.env.MESSAGE_STYLE === "uppercase"){
-//         // res.json({"message": msgObj.message.toUpperCase()});
-//         res.json({
-//             "message": "HELLO JSON"
-//         });
-//     } else res.json({"message": "Hello json"});
-// });
-
-app.get("/json", function(req, res) {
-    res.json({"message": "Hello json"});
+app.get("/json", function(req, res){
+    let msgObj = { message: "Hello json" };
+    if (process.env.MESSAGE_STYLE === "uppercase"){
+        // res.json({"message": msgObj.message.toUpperCase()});
+        res.json({
+            "message": "HELLO JSON"
+        });
+    } else res.json({"message": "Hello json"});
 });
+
+// app.get("/json", function(req, res) {
+//     res.json({"message": "Hello json"});
+// });
 
 
 
