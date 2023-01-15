9efa84269788cc980d9391eb3ec888cc5a2458ef
Hayat <hayatu4islam@yahoo.com>
2023-01-15 18:21:28 +0000

    added route parameter input from the client

--- myApp.js
+++ myApp.js
@@ -16,19 +16,19 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + "/views/index.html");
 });
 
-// app.get("/json", function(req, res){
-//     let msgObj = { message: "Hello json" };
-//     if (process.env.MESSAGE_STYLE === "uppercase"){
-//         // res.json({"message": msgObj.message.toUpperCase()});
-//         res.json({
-//             "message": "HELLO JSON"
-//         });
-//     } else res.json({ "message": "Hello json" });
-// });
-
-app.get("/json", function(req, res) {
-    res.json({"message": "Hello json"});
+app.get("/json", function(req, res){
+    let msgObj = { message: "Hello json" };
+    if (process.env.MESSAGE_STYLE === "uppercase"){
+        // res.json({"message": msgObj.message.toUpperCase()});
+        res.json({
+            "message": "HELLO JSON"
+        });
+    } else res.json({ "message": "Hello json" });
 });
+
+// app.get("/json", function(req, res) {
+//     res.json({"message": "Hello json"});
+// });
 
 function getThePresentTime() {
     return new Date().toString();
@@ -39,7 +39,11 @@ app.get("/now", function(req, res, next){
     next();
 }, function(req, res){
     res.json({ time: req.time });
-})
+});
+
+app.get("/:word/echo", function(req, res){
+    res.send({ echo: req.params.word });
+});
 
 
 
